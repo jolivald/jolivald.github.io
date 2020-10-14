43a48445db13f00058039d97dd6cef782a142949
JJonathan <jolivald.jonathan@gmail.com>
2020-10-14 09:59:30 +0200

    Recreate layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,29 @@
-//jQuery(function($) {
 document.addEventListener('DOMContentLoaded', () => {
   const $bodyEl = document.body;
   const $sidedrawerEl = document.getElementById('sidedrawer');
-
-  function showSidedrawer() {
+  const showSidedrawer = () => {
     // show overlay
-    var options = {
-      onclose: function() {
+    const options = {
+      onclose: () => {
         $sidedrawerEl.classList.remove('active');
         $bodyEl.appendChild($sidedrawerEl);
       }
     };
-
     const $overlayEl = mui.overlay('on', options);
-
     // show element
     $overlayEl.appendChild($sidedrawerEl);
     setTimeout(() => {
       $sidedrawerEl.classList.add('active');
     }, 20);
-  }
+  };
 
-
-  function hideSidedrawer() {
+  const hideSidedrawer = () => {
     $bodyEl.classList.toggle('hide-sidedrawer');
   }
 
   document.querySelectorAll('.js-show-sidedrawer')
     .forEach(trigger => trigger.addEventListener('click', showSidedrawer));
+  
   document.querySelectorAll('.js-hide-sidedrawer')
     .forEach(trigger => trigger.addEventListener('click', hideSidedrawer));
 });
